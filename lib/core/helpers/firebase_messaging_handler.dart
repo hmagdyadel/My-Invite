b412673577be7df3d83e196bdf46df9c3d050e74
Haitham Magdy <hmagdyadel@gmail.com>
2025-02-12 12:07:15 +0200

    change the loader style and make the notification to go to calender screen

--- lib/core/helpers/firebase_messaging_handler.dart
+++ lib/core/helpers/firebase_messaging_handler.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:app/core/helpers/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
