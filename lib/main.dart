3357b4794f81b35e834fddcfd2a6fe816a639a87
Haitham Magdy <hmagdyadel@gmail.com>
2025-03-16 14:42:01 +0200

    handle the scan QR code Back

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'dart:ui';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -9,7 +8,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'core/di/dependency_injection.dart';
 import 'core/services/new_notification_service.dart';
-import 'error_screen.dart';
 import 'features/event_calender/logic/event_calender_cubit.dart';
 import 'my_invite.dart';
 
