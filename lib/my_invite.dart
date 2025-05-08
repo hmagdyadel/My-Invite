43f9ab522dbd406d154038cce778f6cd21d34840
haithammagdy <hmagdyadel1@gmail.com>
2025-05-08 07:18:27 +0300

    try to add no internet connections widget

--- lib/my_invite.dart
+++ lib/my_invite.dart
@@ -2,12 +2,14 @@ import 'package:app/core/helpers/extensions.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-//import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 import 'core/dimensions/dimensions.dart';
 import 'core/routing/app_router.dart';
 import 'core/routing/routes.dart';
 import 'core/services/navigation_service.dart';
+import 'core/theming/colors.dart';
+import 'core/widgets/subtitle_text.dart';
 
 class MyInvite extends StatelessWidget {
   const MyInvite({super.key});
@@ -21,7 +23,7 @@ class MyInvite extends StatelessWidget {
         canPop: false,
         onPopInvokedWithResult: (didPop, result) {
           final NavigatorState navigator =
-              NavigationService.navigatorKey.currentState!;
+          NavigationService.navigatorKey.currentState!;
           if (navigator.canPop()) {
             navigator.pop(result);
           } else {
@@ -40,42 +42,64 @@ class MyInvite extends StatelessWidget {
           locale: context.locale,
           navigatorKey: NavigationService.navigatorKey,
           builder: (context, widget) {
-            // return FutureBuilder<bool>(
-            //   future: InternetConnectionChecker.instance.hasConnection,
-            //   builder: (context, initialSnapshot) {
+            return FutureBuilder<bool>(
+              future: InternetConnectionChecker.instance.hasConnection,
+              builder: (context, initialSnapshot) {
                 // If we're still checking the initial connection, show the regular app
-                //if (!initialSnapshot.hasData) {
+                if (!initialSnapshot.hasData) {
                   return MediaQuery(
                     data: MediaQuery.of(context).copyWith(
                       textScaler: TextScaler.noScaling,
                     ),
                     child: widget!,
                   );
-              //  }
+                }
 
-                // return StreamBuilder<InternetConnectionStatus>(
-                //   stream: InternetConnectionChecker.instance.onStatusChange,
-                //   initialData: initialSnapshot.data == true
-                //       ? InternetConnectionStatus.connected
-                //       : InternetConnectionStatus.disconnected,
-                //   builder: (context, snapshot) {
-                //     final isConnected =
-                //         snapshot.data == InternetConnectionStatus.connected;
-                //
-                //     if (isConnected) {
-                //       return MediaQuery(
-                //         data: MediaQuery.of(context).copyWith(
-                //           textScaler: TextScaler.noScaling,
-                //         ),
-                //         child: widget!,
-                //       );
-                //     } else {
-                //       return const NoInternetWidget();
-                //     }
-                //   },
-                // );
-           //   },
-            //);
+                return StreamBuilder<InternetConnectionStatus>(
+                  stream: InternetConnectionChecker.instance.onStatusChange,
+                  initialData: initialSnapshot.data == true ? InternetConnectionStatus.connected : InternetConnectionStatus.disconnected,
+                  builder: (context, snapshot) {
+                    final isConnected = snapshot.data == InternetConnectionStatus.connected;
+
+                    return MediaQuery(
+                      data: MediaQuery.of(context).copyWith(
+                        textScaler: TextScaler.noScaling,
+                      ),
+                      child: Scaffold(
+                        // backgroundColor: Colors.yellow,
+                        body: Column(
+                          children: [
+                            if (!isConnected)
+                              Container(
+                                //  padding: EdgeInsets.only(top: edge * 2.4, bottom: edge * 0.5),
+                                decoration: BoxDecoration(
+                                  color: errorColor.withAlpha(180),
+                                  //borderRadius: BorderRadius.circular(50),
+                                ),
+                                width: width.w,
+                                child: Column(
+                                  children: [
+                                    SizedBox(height:  edge * 2.4),
+                                    SubTitleText(
+                                      text: 'no_internet'.tr(),
+                                      align: TextAlign.center,
+                                      fontSize: 14,
+                                      color: whiteSmokeColor,
+                                    ),
+                                    SizedBox(height:  edge * 0.5),
+
+                                  ],
+                                ),
+                              ),
+                            Expanded(child: widget!),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
           },
         ),
       ),
