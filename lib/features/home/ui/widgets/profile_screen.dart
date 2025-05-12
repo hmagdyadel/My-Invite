7916c94dcaa74b51232dd9c7c2fbda8d1288d661
Haitham Magdy <hmagdyadel@gmail.com>
2025-05-12 14:55:22 +0300

    change "scan_failed" text

--- lib/features/home/ui/widgets/profile_screen.dart
+++ lib/features/home/ui/widgets/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:app/core/widgets/public_appbar.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:easy_localization/easy_localization.dart';
